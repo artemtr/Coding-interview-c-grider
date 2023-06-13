d68ca21dd5e7d91da490e41fc8e6caa7ae1bdba1
Artem Travlo <info.travlo@gmail.com>
2023-06-13 16:18:10 +0300

    maxchar done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let ob = {};
+  let max = 1;
+  let value = '';
+
+  for (char of str) {
+    if (ob[char]) {
+      ob[char]++;
+    } else {
+      ob[char] = 1;
+    }
+  }
+  for (key in ob) {
+    if (ob[key] >= max) {
+      max = ob[key];
+      value = key;
+    }
+  }
+  return value;
+}
 
 module.exports = maxChar;
