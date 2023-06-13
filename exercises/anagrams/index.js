542c55ce09db159c6abd575b8c735600577737f7
Artem Travlo <info.travlo@gmail.com>
2023-06-13 21:28:33 +0300

    anagrams done

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,38 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let str1 = stringA.replace(/[^\w]/g, '');
+  const obj1 = {};
+  for (const letter of str1) {
+    if (obj1.hasOwnProperty(letter)) {
+      obj1[letter]++;
+    } else {
+      obj1[letter] = 1;
+    }
+  }
+
+  let str2 = stringB.replace(/[^\w]/g, '');
+  const obj2 = {};
+  for (const letter of str2) {
+    if (obj2.hasOwnProperty(letter)) {
+      obj2[letter]++;
+    } else {
+      obj2[letter] = 1;
+    }
+  }
+
+  if (Object.entries(obj1).length !== Object.entries(obj2).length) {
+    console.log('Not');
+  }
+  let check = 'Anagram';
+  for (const key in obj1) {
+    if (!obj2.hasOwnProperty(key) || obj1[key] !== obj2[key]) {
+      check = 'not anagram';
+      break;
+    }
+  }
+  console.log(check);
+}
 
 module.exports = anagrams;
